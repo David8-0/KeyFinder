f48a524dbff179d1e7a3568571911aa003cabac0
David Ayad <david.ayad@trianglz.com>
2025-06-03 01:24:27 +0300

    handle images upload

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const helmet = require('helmet');
 const authRoutes = require('./routes/authRoute.js'); 
 const userRoutes = require('./routes/userRoute.js');
 const projectRoutes = require('./routes/projectRoute.js'); 
+const imagesRoutes = require('./routes/imagesRoutes.js');
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
 
 // Mount auth routes under /api/auth
 app.use('/api/auth', authRoutes);
@@ -19,6 +23,9 @@ app.use('/api/users', userRoutes);
 
 // Mount project routes under /api/projects
 app.use('/api/projects', projectRoutes);
+
+// Mount images routes under /api/images
+app.use('/api/images', imagesRoutes);
 
 // Health check or other routes
 app.get('/', (req, res) => {
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+const deleteFile = async (deleteFilPath)=>{
+    fs.access(deleteFilPath, fs.F_OK, async (err, ac) => {
+            await fs.unlink(deleteFilPath, (ferr, fc) => {
+                    return true;
+            });
+        
+    });
+}
+
+
+const deleteImagesfromServer = async (filesPath)=>{
+  if(!filesPath.length || (!Array.isArray(filesPath))) throw new Error("you must provide an array of files path");
+  const publicDir = path.join(__dirname, '..', 'public');
+  filesPath.forEach(element => {
+      const splitedPath = element.split('/');
+      const deleteFilPath = `${publicDir}/images/${splitedPath[splitedPath.length-2]}/${splitedPath[splitedPath.length-1]}`;
+      deleteFile(deleteFilPath);
+  });   
+}
+
+
+
+
 const successResponse = (data, statusCode = 200) => ({
   success: true,
   data
@@ -13,5 +38,6 @@ const errorResponse = (message, statusCode = 500) => ({
 
 module.exports = {
   successResponse,
-  errorResponse
+  errorResponse,
+  deleteImagesfromServer
 };
